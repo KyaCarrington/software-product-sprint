74744b0490115af3403f395ffd01fa9a7b3073ed
Kya <kyamcarrington@gmail.com>
2021-03-18 20:31:41 +0000

    Added a datastore to store inputted contact information

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,11 +1,14 @@
 package com.google.sps.servlets;
-
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
 
@@ -13,14 +16,19 @@ public class FormHandlerServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the value entered in the form.
     String textValue = request.getParameter("text-input");
+    long timestamp = System.currentTimeMillis();
+    
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Text");
+    
+    FullEntity taskEntity =
+    Entity.newBuilder(keyFactory.newKey())
+        .set("text-input", textValue)
+        .set("timestamp", timestamp)
+        .build();
+    datastore.put(taskEntity); 
 
-    // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
-
-    // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
-
-  
+    response.sendRedirect("/index.html");
 }
 
 
